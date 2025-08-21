05d943c9514fb67300dbe2e86c2909dee347cdad
Aitchel17 <lhs4869@gmail.com>
2025-08-20 23:38:26 -0400

    loadinfo done

--- io_hlee/io_loadinfo_hl.m
+++ io_hlee/io_loadinfo_hl.m
@@ -1,11 +1,6 @@
 function info = io_loadinfo_hl(infotxtdirectory)
-    info.infoname = dir(fullfile(info.analyzefolder, '*_info.txt'));
-        info.infoname = info.infoname.name;
-        tmp.infopath = fullfile(info.analyzefolder, info.infoname);
-        tmp.info_table = readtable(tmp.infopath);
-
-        for i = 1:height(tmp.info_table)
-            info.(tmp.info_table.Field{i}) = tmp.info_table.Value{i}; 
+        info_table = readtable(infotxtdirectory);
+        for i = 1:height(info_table)
+            info.(info_table.Field{i}) = info_table.Value{i}; 
         end
-        obj.info = info;
     end
