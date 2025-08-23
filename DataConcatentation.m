b2b38253a9a054db08c1de5e9e2cbe6f93477eb9
Aitchel17 <lhs4869@gmail.com>
2025-08-23 12:55:40 -0400

    temporary patch for t_axis, its better to lee to make .mat priamry sleepscore data set that matches with fiberphotometry data

--- DataConcatentation.m
+++ DataConcatentation.m
@@ -1,16 +1,22 @@
 function FinalSleepData = DataConcatentation(FileDirectory)
     % <Documentation>
+        % 250822_code update
+        % This is temporary data feedstruct since Data input output (H.Lee) --> Figure plot (Julio) --> User interface (Jake)
+        % The issue was raw ECoG data is not useful and spectral analysis of ECoG give t axis while hql5715 did not provide time axis for raw data set.
+        % Therfore at the temporary UI made by Jake mismatched
+        % 250823_ update, perhaps its better to H.Lee to follow Fiber datastructure rather than integrating preprocessing step into current step
+            % this function would be better for just UI development before Julio's engagement delayed by Lee's interpretation of Kevin's directory and data management
         % DataConcatentation()
         %   
         %   Created by: jsl5865
-        %   
+        %   Filled by : hql5715
         % Syntax:
         %   
         % Description:
         %   
-        % Input:
+        % Input: FileDirectory
         %   
-        % Output:
+        % Output: datastruct containing 
         %   
     % <End Documentation>
 
@@ -22,17 +28,46 @@ function FinalSleepData = DataConcatentation(FileDirectory)
         %                               .f
         %                               .spectrum
         %                        )
+    FinalSleepData = struct(); % output
+    sleepscore_rawData = io_main(FileDirectory);
+    Analogfreq = str2double(sleepscore_rawData.analog.info.analogfreq(1:end-2));
+        feedstruct(sleepscore_rawData.analog.data,'raw_ECoG')
+% ECoG processing
+        [raw_ECoG,goflag] = feedstruct(sleepscore_rawData.analog.data,'raw_ECoG');
+        if goflag
+            EcogData = ecogprocess(Analogfreq, raw_ECoG);
+            FinalSleepData.EcogData.t = EcogData.t_axis;
+            FinalSleepData.EcogData.f = EcogData.f_axis;
+            FinalSleepData.EcogData.spectrum = EcogData.log_norm_spectrum;
+        end
+% EMG processing
+        [raw_emg, goflag] = feedstruct(sleepscore_rawData.analog.data,'raw_EMG');
+        if goflag
+            FinalSleepData.raw_emg = raw_emg;
+            FinalSleepData.raw_emg_t = maketimeaxis(size(raw_emg,2), Analogfreq);
+        end
+% Force processing
+        [raw_force, goflag] = feedstruct(sleepscore_rawData.analog.data,'raw_Force');
+        if goflag
+            FinalSleepData.raw_force = raw_force;
+            FinalSleepData.raw_force_t = maketimeaxis(size(raw_force,2), Analogfreq);
+        end
 
-    AnalogData = io_main(FileDirectory);
-    EcogData = ecogprocess(AnalogData.analog.info.analogfreq, AnalogData.analog.data.raw_ECoG);
-    EcogData.t = EcogData.t_axis;
-    EcogData.f = EcogData.f_axis;
-    EcogData.spectrum = EcogData.log_norm_spectrum;
+end
 
-    FinalSleepData.EMG = AnalogData.analog.data.raw_EMG;
-    FinalSleepData.Force = AnalogData.analog.data.raw_Force;
-    FinalSleepData.ECOG.t = EcogData.t;
-    FinalSleepData.ECOG.f = EcogData.f;
-    FinalSleepData.ECOG.spectrum = EcogData.log_norm_spectrum;
 
+function [output,existflag] = feedstruct(data_struct, fieldname)
+    if isfield(data_struct, fieldname)
+        output = data_struct.(fieldname);
+        existflag = true;
+    else
+        fprintf('field "%s" does not exist.\n', fieldname)
+        output = struct();
+        existflag = false;
+    end
+end
+
+function taxis = maketimeaxis(n_datapoints,frequency)
+    tmax = (n_datapoints -1 ) / frequency;
+    taxis = linspace(0,tmax,n_datapoints);
 end
