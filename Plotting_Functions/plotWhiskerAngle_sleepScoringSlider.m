8d3f1bdbc473c8c8c5f61991c64f56479a9ed605
JF <floresjulio27@gmail.com>
2025-08-27 22:24:27 -0400

    Whisker angle plotting for sleep scoring slider function

---
+++ Plotting_Functions/plotWhiskerAngle_sleepScoringSlider.m
@@ -0,0 +1,29 @@
+function [ax3,p3] = plotWhiskerAngle_sleepScoringSlider(procDataFileID)
+
+%Input: procDataFileID is defined outside the function in a loop that goes through the four hours of recording
+
+% load the necessary data
+load(procDataFileID)
+%[animalID,fileDate,fileID] = GetFileInfo_FP(procDataFileID); % this only if we want the file name ID in the tittle of the figure (optional)
+
+% setup butterworth filter coefficients for a 1 Hz and 10 Hz lowpass based on the sampling rate
+[z1,p1,k1] = butter(4,10/(ProcData.notes.dsFs/2),'low');
+[sos1,g1] = zp2sos(z1,p1,k1);
+
+% whisker angle data
+filteredWhiskerAngle = filtfilt(sos1,g1,ProcData.data.whiskerAngle);
+
+%Make the plot 
+ax3 = subplot(7,1,3); %Just leaving seven as original bur probably this might change 
+p3 = plot((1:length(filteredWhiskerAngle))/ProcData.notes.dsFs,-filteredWhiskerAngle,'color',[0.8 0.1 0.4],'LineWidth',1);
+ylabel('Angle (deg)')
+xlim([0,ProcData.notes.trialDuration_sec])
+legend(p3,'Whisker Angle')
+axis tight
+
+%optional
+set(gca,'TickLength',[0,0])
+set(gca,'Xticklabel',[])
+set(gca,'box','off')
+
+end
