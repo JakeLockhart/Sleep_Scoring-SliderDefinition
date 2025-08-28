6595305cdf0aa9b472f5ab7a5c9b3f7572d94f07
JF <floresjulio27@gmail.com>
2025-08-27 22:03:19 -0400

    EMG plotting for sleep scoring slider function

---
+++ Plotting_Functions/plotEMGPower_sleepScoringSlider.m
@@ -0,0 +1,28 @@
+function [ax2,p2] = plotEMGPower_sleepScoringSlider(procDataFileID)
+
+%Input: procDataFileID is defined outside the function in a loop that goes through the four hours of recording
+
+% load the necessary data
+load(procDataFileID)
+%[animalID,fileDate,fileID] = GetFileInfo_FP(procDataFileID); % this only if we want the file name ID in the tittle of the figure (optional)
+
+% EMG data for plotting 
+EMG = ProcData.data.EMG.emg;
+
+%Make the plot 
+ax2 = subplot(7,1,2); %Just leaving seven as original bur probably this might change 
+p2 = plot((1:length(EMG))/ProcData.notes.dsFs,EMG,'color','k','LineWidth',1);
+ylabel('EMG Power (V^2)')
+xlim([0,ProcData.notes.trialDuration_sec])
+legend(p2, 'EMG Power', Location='eastoutside')
+set(gca,'TickLength',[0,0])
+set(gca,'Xticklabel',[])
+set(gca,'box','off')
+axis tight
+
+%optional
+set(gca,'TickLength',[0,0])
+set(gca,'Xticklabel',[])
+set(gca,'box','off')
+axis tight
+end
