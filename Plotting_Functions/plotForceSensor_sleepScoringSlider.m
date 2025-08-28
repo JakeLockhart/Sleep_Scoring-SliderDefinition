4a0d6f14a1293a8dd721aa42109211b91b8da06d
JF <floresjulio27@gmail.com>
2025-08-27 21:02:06 -0400

    Created function to plot force data in the same way as legacy code to be implemented in the UI_SleepScoring app. More functions to follow

---
+++ Plotting_Functions/plotForceSensor_sleepScoringSlider.m
@@ -0,0 +1,29 @@
+function [ax1,p1] = plotForceSensor_sleepScoringSlider(procDataFileID)
+
+%Input: procDataFileID is defined outside the function in a loop that goes through the four hours of recording
+
+% load the necessary data
+load(procDataFileID)
+%[animalID,fileDate,fileID] = GetFileInfo_FP(procDataFileID); % this only if we want the file name ID in the tittle of the figure (optional)
+
+% setup butterworth filter coefficients for a 1 Hz and 10 Hz lowpass based on the sampling rate
+[z1,p1,k1] = butter(4,10/(ProcData.notes.dsFs/2),'low');
+[sos1,g1] = zp2sos(z1,p1,k1);
+
+% force sensor data for plotting 
+filtForceSensor = filtfilt(sos1,g1,ProcData.data.forceSensor);
+
+%Make the plot 
+ax1 = subplot(7,1,1); %Just leaving seven as original bur probably this might change 
+p1 = plot((1:length(filtForceSensor))/ProcData.notes.dsFs,filtForceSensor,'Color', 'k','LineWidth',1);
+title('Behavioral characterization and sleep scoring')
+ylabel('Force Sensor (V)')
+xlim([0,ProcData.notes.trialDuration_sec])
+legend((p1),'Force Sensor')
+
+%optional
+set(gca,'TickLength',[0,0])
+set(gca,'Xticklabel',[])
+set(gca,'box','off')
+axis tight
+end
